a2303fb901dcb9a1e3a6d896817994c974268bd1
Thomas Bozeman <thomas.bozeman@cgifederal.com>
2025-03-05 15:14:41 -0600

    other updates

--- install.py
+++ install.py
@@ -7,13 +7,36 @@ import argparse
 import logging
 import os
 import shutil
+from collections.abc import Callable, Sequence
+from dataclasses import dataclass
 from pathlib import Path
-from typing import Callable
 
 ROOT = Path(__file__).absolute().parent
 DESCRIPTION = __doc__
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 logger = logging.getLogger(__name__)
+
+
+@dataclass
+class Settings:
+    """A dataclass for holding script settings."""
+
+    # the root of the config source
+    config_root: Path = Path(ROOT)
+    # the home directory to install to
+    home_dir: Path = Path.home()
+    # the config directory to install to
+    config_dir: Path = Path.home() / '.config'
+    # the ssh directory to install to
+    ssh_dir: Path = Path.home() / '.ssh'
+    # whether to perform any actions
+    dry_run: bool = False
+    # whether to create relative symbolic links
+    relative_links: bool = False
+    # whether to create hard links
+    hard_links: bool = False
+    # what to do for file conflicts
+    on_conflict: Literal["skip", "rename", "overwrite"] = "skip"
 
 
 def remove_path(path: Path) -> None:
@@ -24,7 +47,7 @@ def remove_path(path: Path) -> None:
         path.unlink()
 
 
-def preserve_path(path: Path, max_backup_count: int = 10) -> None:
+def rename_path(path: Path, max_backup_count: int = 10) -> None:
     """Rename the file or directory at the given path as a backup."""
     suffixes = [".bak"] + [f".{i}.bak" for i in range(1, max_backup_count)]
     for suffix in suffixes:
@@ -48,9 +71,7 @@ def relative_to(path: Path, target: Path) -> Path:
 def link_files(
     source_dir: Path,
     dest_dir: Path,
-    relative_links: bool,
-    preserve_existing: bool,
-    dryrun: bool,
+    settings: Settings,
     transformer: Callable[[Path], Path] | None = None,
 ):
     """Link files from one directory to another."""
@@ -59,12 +80,14 @@ def link_files(
         return
 
     if not dest_dir.exists():
-        logger.info("creating %s", dest_dir)
-        if not dryrun:
+        if settings.dry_run:
+            logger.info("would create %s", dest_dir)
+        else:
+            logger.info("creating %s", dest_dir)
             dest_dir.mkdir(parents=True)
 
     for path in source_dir.iterdir():
-        if relative_links:
+        if settings.relative_links:
             target = relative_to(dest_dir, path)
         else:
             target = path
@@ -73,113 +96,120 @@ def link_files(
         if transformer:
             link = transformer(link)
 
-        if link.is_symlink() and link.readlink() == target:
-            if dryrun:
+        if (link.exists() and link.samefile(target)) or (link.is_symlink() and link.readlink() == target):
+            # same file!
+            if settings.dry_run:
                 logger.info("would skip %s", link)
             else:
                 logger.info("skipping %s", path)
             continue
 
         if link.exists() or link.is_symlink():
-            if preserve_existing:
-                if dryrun:
-                    logger.info("would preserve %s", link)
+            # conflict!
+            if settings.on_conflict == "overwrite":
+                if settings.dry_run:
+                    logger.info("would overwrite %s", link)
                 else:
-                    logger.info("preserving %s", link)
-                    preserve_path(link)
-            else:
-                if dryrun:
-                    logger.info("would destroy %s", link)
-                else:
-                    logger.info("destroying %s", link)
+                    logger.info("removing %s", link)
                     remove_path(link)
+            elif settings.on_conflict == "rename":
+                if settings.dry_run:
+                    logger.info("would rename %s", link)
+                else:
+                    logger.info("renaming %s", link)
+                    rename_path(link)
+            else:
+                if settings.dry_run:
+                    logger.info("would skip %s", link)
+                else:
+                    logger.info("skipping %s", link)
+                continue
 
         logger.info("%s -> %s", link, target)
-        if not dryrun:
-            link.symlink_to(target)
+        if not settings.dry_run:
+            if settings.hard_links:
+                if path.is_dir():
+                    raise NotImplementedError("we don't have that yet :(", link, target)
+                link.hardlink_to(target)
+            else:
+                link.symlink_to(target)
 
 
-def config_install(
-    config_root: os.PathLike,
-    homedir: os.PathLike,
-    configdir: os.PathLike,
-    sshdir: os.PathLike,
-    relative_links: bool = True,
-    preserve_existing: bool = False,
-    dryrun: bool = True,
-) -> None:
-    """
-    Install config files.
-
-    :param config_root: root of the `config` project
-    :param homedir: the home directory to install to
-    :param configdir: the config directory to install to
-    :param sshdir: the ssh directory to install to
-    :param relative: create relative symlinks, defaults to True
-    :param preserve: rename existing files to `*.bak`, defaults to False
-    :param dryrun: look but don't touch, defaults to False
-    """
-
-    config_root = Path(config_root).absolute()
-    homedir = Path(homedir).absolute()
-    configdir = Path(configdir).absolute()
-    sshdir = Path(sshdir).absolute()
-    settings: dict = {
-        "relative_links": relative_links,
-        "preserve_existing": preserve_existing,
-        "dryrun": dryrun,
-    }
+def config_install(settings: Settings) -> None:
+    """Install all my config files."""
 
     # first dotfiles
     link_files(
-        config_root.joinpath("dotfiles"),
-        homedir,
-        **settings,
+        settings.config_root / "dotfiles",
+        settings.home_dir,
+        settings,
         transformer=(lambda p: p.with_name("." + p.name)),
     )
 
     # then configfiles
-    link_files(config_root.joinpath("configfiles"), configdir, **settings)
+    link_files(settings.config_root / "configfiles", settings.config_dir, settings)
 
     # then sshfiles
-    link_files(config_root.joinpath("sshfiles"), sshdir, **settings)
+    link_files(settings.config_root / "sshfiles", settings.ssh_dir, settings)
 
 
-def main() -> None:
+def get_settings() -> Settings:
+    
     parser = argparse.ArgumentParser(description=DESCRIPTION)
     parser.add_argument(
-        "-r",
+        "-R",
         "--relative-links",
-        "--relative",
         action="store_true",
         help="make symlinks relative",
     )
     parser.add_argument(
-        "-p",
-        "--preserve-existing",
-        "--preserve",
-        action="store_true",
-        help="preserve existing files as backups",
-    )
-    parser.add_argument(
-        "-d",
-        "--dryrun",
+        "-D",
         "--dry-run",
         action="store_true",
         help="make no changes; describe what actions would be taken",
     )
+    parser.add_argument(
+        "-H",
+        "--hard-links",
+        action="store_true",
+        help="use hard links instead of symbolic links",
+    )
+
+    conflict_group = parser.add_mutually_exclusive_group()
+    conflict_group.add_argument(
+        "-r",
+        "--rename-conflicts",
+        action="store_const",
+        dest="conflict",
+        const="rename",
+        help="rename conflicting files as backups",
+    )
+    conflict_group.add_argument(
+        "-s",
+        "--skip-conflicts",
+        action="store_const",
+        dest="conflict",
+        const="skip",
+        help="skip conflicting files",
+    )
+    conflict_group.add_argument(
+        "-o",
+        "--overwrite-conflicts",
+        action="store_const",
+        dest="conflict",
+        const="overwrite",
+    )
+
     args = parser.parse_args()
 
-    config_install(
-        config_root=ROOT,
-        homedir=Path.home(),
-        configdir=Path.home().joinpath(".config"),
-        sshdir=Path.home().joinpath(".ssh"),
+    return Settings(
         relative_links=args.relative_links,
-        preserve_existing=args.preserve_existing,
-        dryrun=args.dryrun,
+        dry_run=args.dry_run,
+        hard_links=args.hard_links,
+        on_conflict=args.conflict
     )
 
 
 if __name__ == "__main__":
-    main()
+    settings = get_settings()
+    config_install(settings)
